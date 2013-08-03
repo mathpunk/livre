236389499f61f7452173af4402705473c354830a
Thomas Henderson <tom@mathpunk.net>
2013-08-03 14:15:21 -0700

    Getting closer to a working inventory

--- src/livre/data/clone-db.clj
+++ src/livre/data/clone-db.clj
@@ -1,7 +1,8 @@
+; Forked and mutated from http://blog.malcolmsparks.com/?p=67 
 
+(ns data.inventory.vimwiki)
+; data protocols/records
 (defprotocol Transaction (update [_ state]))
-
-Here’s some example transactions we want to perform. For this article let’s imagine we’re creating a database of user accounts. We’ll just create two transaction types- creating users and deleting them. Let’s record the name and email address, plus the date they joined or left.
 
 (defrecord CreateUser [name email start-date]
   Transaction
@@ -15,11 +16,17 @@ Here’s some example transactions we want to perform. For this article let’s 
     (update-in db [:users]
       (fn [coll] (remove (fn [r] (= (:email r) email)) coll)))))
 
-Data is important. We’ll want to keep records of these transactions.
+; transaction protocols/records
 
 (defprotocol TransactionLog (record [_ tx]))
 
-Here’s an implementation that uses an agent to write the records to a print-stream.
+; Here’s an implementation that uses an agent to write the records to a print-stream.
+;
+; The agent will hold the print-stream. Agents are good for I/O because messages
+; to them only get delivered if the transaction completes its update succesfully.
+; If a retry is needed the message doesn’t get delivered. We let the Clojure prn
+; function figure out how to serialize it. It’s optional, but the
+; actual print statement is in an io! wrapper as a safety check.
 
 (defrecord DefaultTransactionLog [ag]
   TransactionLog
@@ -29,9 +36,10 @@ Here’s an implementation that uses an agent to write the records to a print-st
         (binding [*out* out *flush-on-newline* true]
           (io! (prn tx)) out)))))
 
-The agent will hold the print-stream. Agents are good for I/O because messages to them only get delivered if the transaction completes its update succesfully. If a retry is needed the message doesn’t get delivered. We let the Clojure prn function figure out how to serialize it. It’s optional, but I’ve wrapped the actual print statement in an io! wrapper as a safety check.
-
-Having written the records we’ll need some way of reading them back. We’ll just use the standard Clojure reader for this. This function returns a list of all the transactions in a file.
+; reading the transactions in a file
+; Having written the records we’ll need some way of reading them back. We’ll
+; just use the standard Clojure reader for this. This function returns a list
+; of all the transactions in a file.
 
 (defn read-transactions [f]
   (if-not (.exists f) []
@@ -40,7 +48,12 @@ Having written the records we’ll need some way of reading them back. We’ll j
         (repeatedly 
           (fn [] (read rdr false nil)))))))
 
-Now we move on to the database state. Let’s decorate the transaction log with something that will hold this state. As transactions are added to it they will update the in-memory state and be recorded on disk. This can support the TransactionLog protocol as well. The state will be a Clojure ref. The delegate will be a backing transaction log.
+; database state
+; Now we move on to the database state. Let’s decorate the transaction log with
+; something that will hold this state. As transactions are added to it they
+; will update the in-memory state and be recorded on disk. This can support the
+; TransactionLog protocol as well. The state will be a Clojure ref. The
+; delegate will be a backing transaction log.
 
 (defrecord Database [state delegate]
   TransactionLog
@@ -48,8 +61,10 @@ Now we move on to the database state. Let’s decorate the transaction log with 
     (dosync
      (alter state (partial update tx))
      (record delegate tx))))
-
-Now we’re ready to create our database with the Clojure ref and backing transaction log. We’ll initialize the ref with a result of updating an empty map with a series of transactions read from our file.
+; creating the database 
+; Now we’re ready to create our database with the Clojure ref and backing  transaction log. 
+; We’ll initialize the ref with a result of updating an empty
+; map with a series of transactions read from our file.
 
 (def db 
    (Database.
@@ -61,17 +76,15 @@ Now we’re ready to create our database with the Clojure ref and backing transa
                 (clojure.java.io/file "my.db.clj")
                 :append true)))))
 
-Remember those transactions we coded earlier? Let’s add some convenience functions that will create and apply these transactions :-
-
-(defn create-user [name email]
-    (record db (CreateUser. name email (java.util.Date.))))
+; convenience functions
 
 (defn delete-user [email]
     (record db (DeleteUser. email (java.util.Date.))))
 
-Notice how we’re creating java.util.Date instances.
+(defn create-user [name email]
+    (record db (CreateUser. name email (java.util.Date.))))
 
-Now, let’s test it.
+; tests
 
 (create-user "Bob" "bob@example.org")
 (create-user "Alice" "alice@example.org")
@@ -79,11 +92,43 @@ Now, let’s test it.
 (delete-user "alice@example.org")
 
 
-What’s the state of the database?
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; repurposing for Copy records
+;
+(def test-file "/home/thomas/src/livre/resources/material/vimwiki/big-picture.wiki")
+(def test-dir "/home/thomas/src/livre/resources/material/vimwiki/")
+(defn- uuid [] (str (java.util.UUID/randomUUID)))
+
+(defrecord CreateCopy [file]
+  Transaction
+  (update [this db]
+    (update-in db [:copy] conj
+      {
+        :id (uuid)
+        :history [
+            { :timestamp (.lastModified file)   ; using modified time as a best guess
+              :diff ""
+            }
+          ]
+        :content (slurp file)                   ; the full text of the file
+        :title  (.getName file)
+        :path (.getParent file)
+      })
+    )
+  )
+
+(defn create-copy [filename]
+  (let [file (new java.io.File filename)]
+    (record db (CreateCopy. file))))
+
+(create-copy test-file)
+
+
+; What’s the state of the database?
 
 (deref (:state db))    
 
-You should see something like this :-
+; You should see something like this :-
 
 {:users 
   ({:name "Carol",
@@ -93,7 +138,17 @@ You should see something like this :-
     :email "bob@example.org",
     :start-date #inst "2012-04-19T16:00:35.901-00:00"})}
 
-What’s in our database file? Below is the raw content. Unlike some database files it’s quite easy to read and even possible to edit by hand if the need arises.
+; Goal data: something like--
+{:copy 
+  ({:name "big-picture.wiki"
+    :content "<<some stuff>>"
+    :history {:timestamp #inst "<<m-time>>", :diff ""}
+    :path "~/src/livre/src/livre/data/vimwiki"})}
+
+
+; What’s in our database file? Below is the raw content. Unlike some database
+; files it’s quite easy to read and even possible to edit by hand if the need
+; arises.
 
 #blog.CreateUser{:name "Bob", :email 
 "bob@example.org", :start-date #inst 
@@ -107,14 +162,3 @@ What’s in our database file? Below is the raw content. Unlike some database fi
 #blog.DeleteUser{:email "alice@example.org", 
 :remove-date #inst 
 "2012-04-19T16:00:35.904-00:00"}
-
-Note, unless you aren’t using Clojure 1.4 or above you won’t see the #inst tags!
-
-Notice how Clojure is writing out and reading back the java.util.Date instance. You can also register your own Java types to it through the *data-readers* dynamic binding (more details in the Clojure 1.4 release notes). This makes things really easy to create some fairly complex atomic transactions.
-
-Now try recompiling – you should find the database is restored. You can also try removing the user. The transaction log will store both the create-user event and the delete-user event, both with timings.
-
-That’s around 30 lines of code to code a transactional database, and a few more lines to code some custom transactions.
-
-You don’t have to limit yourself to a map as your state. At Deutsche Bank we’ve used a similar design but with RDF triple-stores that are queryable in a datalog syntax. A key benefit with using Clojure’s persistent data-structures for simple in-memory datastores is that you don’t have to worry about locks or concurrent updates.
-
