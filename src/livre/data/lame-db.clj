96c4b7d35fab5f06f03dc73b1e0f15917504fa95
Thomas Henderson <tom@mathpunk.net>
2013-08-03 16:05:00 -0700

    Combine inventory and lame-db files.

--- src/livre/data/lame-db.clj
+++ src/livre/data/lame-db.clj
@@ -88,7 +88,21 @@
     )
   )
 
-;;;;;;; TESTING
-(def test-file "/home/thomas/src/livre/resources/material/vimwiki/big-picture.wiki")
-(def test-dir "/home/thomas/src/livre/resources/material/vimwiki/")
-(create-copy test-file)
+; notes for inventory
+; TRANSFORM INTO DATA STRUCTURES
+(def archive "/home/thomas/cerebra/wiki/wiki-archive") ; dead
+
+
+; Transform all archive's files into Files (not Directories) and map them.
+(defn records [archive]
+    (map record 
+         (filter #(.isFile %) 
+                  (file-seq (clojure.java.io/file archive)))
+        )
+  )
+
+;;;; testing Copy DB
+(defn test-copy [ ]
+  (let [test-file "/home/thomas/src/livre/resources/material/vimwiki/big-picture.wiki"]
+    (create-copy test-file)))
+(test-copy)
