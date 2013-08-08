215fc5cc50dca8b73b3e9e84dd7aed90b258b53b
Thomas Henderson <tom@mathpunk.net>
2013-08-07 18:39:43 -0700

    It works

--- src/livre/data/to-edn.clj
+++ src/livre/data/to-edn.clj
@@ -3,27 +3,47 @@
 
 (def config (clojure.edn/read-string  (slurp "/home/thomas/src/livre/config.clj") ))
 
-; k back to doing things
 (defn value-of-a-text-file [file]
      { 
        :title       (.getName file)
        :ws-path     (.getParent file)
-       :ws          (.getName :ws-path)
+       :ws          (.getName (.getParentFile file))
        :content     (slurp file)
        :history     [{:tx (.lastModified file) :diff nil}]
       }
   )
 
-(def transform-text-directory-to-values
-  (let [dir (clojure.java.io/file (config :directory)), 
+(defn transform-text-directory-to-values [dirname]
+  (let [dir (clojure.java.io/file dirname), 
         files (filter #(.isFile %) (file-seq dir))]
-      (set (map value-of-a-text-file files))
+      (set (map value-of-a-text-file (remove #(.isHidden %) files)))
     )
   )
 
+(def all-wiki-data (transform-text-directory-to-values (config :directory)))
 
-(def dead-wiki "/home/thomas/cerebra/wiki/wiki-archive") 
-(def live-pm-wiki "/home/thomas/cerebra/wiki/pm")
+(def dead-wiki-data (transform-text-directory-to-values "/home/thomas/cerebra/wiki/wiki-archive"))
+
+(def live-pm-wiki-data (transform-text-directory-to-values "/home/thomas/cerebra/wiki/pm"))
+
+(take 5 all-wiki-data)
+
+(take 5 dead-wiki-data)
+
+(take 5 live-pm-wiki-data)
+
+
+
+(spit "./src/livre/data/all-data-sample.clj" (take 5 all-wiki-data))
+
+(spit "./src/livre/data/live-pm-sample.clj" (take 5 live-pm-wiki-data))
+
+(spit "./src/livre/data/cut-pm-sample.clj" (take 5 dead-wiki-data))
+
+
+
+
+
 (defprotocol Writespace [x]
   "A namespace for text. Like, a book you're working on, a side project, and letter to a circle. You can add whatever you
   like as you're drafting in this space, but they are membraned off from other text that you are working."
@@ -31,3 +51,10 @@
     (name [ ]     "Writespaces have a name, like a word or a full title." 
     (abbrev [ ]   "Writespaces may have a short-name for referring to."
 )))
+
+
+;;;;;;;;;; OTHER TRANSFORMATIONS
+; = to # to make it more markdowny
+; [[xxx ] ] to subtextons or however we're doing pagelinks
+; ".wiki" as "look i haven't markdowned this"? nooooo
+; some really dumb diffing on very important data
