53b08507f374a5f2665a59bcbad2cfce41b8c13b
Thomas Henderson <tom@mathpunk.net>
2013-08-08 18:18:56 -0700

    Moved material out of resources cause it seemed weird

--- config.clj
+++ config.clj
@@ -1,9 +1,19 @@
 { 
-  :live-directory  nil
-  :directory "/home/thomas/cerebra/wiki"
-  :archives { 
-      :twitter nil
-      :en nil 
-     }
- }
+  :home "/home/thomas/cerebra/wiki"             ; your home is private, obvsly,
+                                                ; but you can let who you want in.
 
+  :writespaces ["pm", "lf5", "livre", "alg"]    ; subdirs in home that are for 
+                                                ; projectifying
+ 
+  :archives {                                   ; to import things that aren't
+                                                ; being developed? or that have
+                                                ; record types set up? I dunno.
+    :directories [
+                    "/home/thomas/cerebra/wiki/wiki-archive/early-pm-wiki"
+                  ; "/home/thomas/cerebra/wiki/wiki-archive/wiki-backup"
+                  ; "/home/thomas/cerebra/wiki/wiki-archive/vim-wiki"
+                  ]
+    :twitter nil
+    :en nil 
+     }
+}
--- resources/material/tweets/tweets, aug 1 2013/data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 11602,
-  "created_at" : "Fri Aug 02 08:43:21 +0000 2013",
-  "lang" : "en"
-}
