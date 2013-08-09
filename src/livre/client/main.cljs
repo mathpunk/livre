29e3614fc3dd4a637607d95e0abd96cfec6d54a3
Thomas Henderson <tom@mathpunk.net>
2013-08-09 14:01:41 -0700

    FUCK YES the client is calling an arbitrary server function

--- src/livre/client/main.cljs
+++ src/livre/client/main.cljs
@@ -6,27 +6,27 @@
 
 ; divisions
 (def $body ($ :body))
-(def $controls ($ :#controls))
+(def $builder ($ :#builder))
 
 ; buttons
 (defpartial button [{:keys [label action param]}]
   [:a.button {:href "#" :data-action action :data-param param} label])
 
 (delegate $body button :click
-                    (fn [e]
-                      (.preventDefault e)
-                      (this-as me
-                               (let [$me ($ me)
-                                     action (data $me :action)
-                                     param (data $me :param)
-                                     params (if (= param "")
-                                              [ ]
-                                              [param])]
-                                 (remotes/remote-callback action params)))))
+      (fn [e]
+        (.preventDefault e)
+        (this-as me
+                  (let [$me ($ me)
+                        action (data $me :action)
+                        param (data $me :param)
+                        params (if (= param "")
+                                [ ]
+                                [param])]
+                    (remotes/remote-callback action params)))))
 
 
 ; building controls
-(append $controls (button {:label "count something" 
+(append $builder (button {:label "count something" 
                            :action "count-something" 
                            :param "40"}))
 
