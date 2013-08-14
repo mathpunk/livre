7c7fe27d22bf8a799d923f59dd8df9418daa54dc
Thomas Henderson <tom@mathpunk.net>
2013-08-14 10:22:05 -0700

    describe algorithm; start chopping

--- src/livre/server/inventory.clj
+++ src/livre/server/inventory.clj
@@ -28,7 +28,6 @@
 ; :title nil
 ; :ws nil
 ; :content {:uri blah} ?
-;
 
 (defn- transform-text-directory-to-values [dirname]
   (let [dir (clojure.java.io/file dirname),
@@ -59,3 +58,82 @@
 (def text-test-dir "/home/thomas/src/livre/material/vimwiki")
 
 (local-dir-to-local-edn text-test-dir "/home/thomas/src/livre/data/test.edn")
+
+; sanity docs
+; * what am i doing?
+; you are...
+; you need to get your documents into the database. 
+; you know how to convert your documents into values.
+; you have a snippet on adding an object id to a document.
+; you made a connection to a database.
+; you have added documents to the database.
+;
+;
+; algorithm:
+;
+; load monger namespace and requirements
+; (remove edn requirement)
+; 
+; turn target directory into values, adding ObjectIDs
+;
+; insert collection
+;
+; verify
+;
+; celebrate
+
+(ns livre.server
+  (:require [monger.core :as mg])
+  (:import [com.mongodb MongoOptions ServerAddress]))
+
+;; given host, given port
+(mg/connect! { :host "localhost" :port 27017 })
+
+;; using MongoOptions allows fine-tuning connection parameters,
+;; like automatic reconnection (highly recommended for production environment)
+(let [^MongoOptions opts (mg/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
+      ^ServerAddress sa  (mg/server-address "127.0.0.1" 27017)]
+  (mg/connect! sa opts))
+
+(get-collection-names)
+
+
+;; localhost, default port
+
+(mg/connect!)
+
+(mg/set-db! (mg/get-db "units"))
+
+(ns livre.server
+    (:use [monger.core :only [connect! connect set-db! get-db]]
+                  [monger.collection :only [insert insert-batch]])
+    (:import [org.bson.types ObjectId]
+                        [com.mongodb DB WriteConcern]))
+
+(insert "documents" { :_id (ObjectId.) :hello "world" })
+
+;; multiple documents at once
+(insert-batch "document" [{ :first_name "John" :last_name "Lennon" }
+; ...is a recommended way of inserting batches of documents (from tens to hundreds
+; of thousands) because it is very efficient compared to sequentially or even
+; concurrently inserting documents one by one.
+                          { :first_name "Paul" :last_name "McCartney" }])
+
+;; without document id (when you don't need to use it after storing the document)
+(insert "document" { :first_name "John" :last_name "Lennon" })
+
+
+
+
+
+
+(ns my.service.server
+  (:require [monger.collection :as mc])
+  (:use monger.operators))
+
+(mc/insert "documents" {:hello  "dolly" })
+(mc/insert "documents" {:hello "nasty" })
+(mc/insert "documents" {:hello "nurse"})
+
+(mc/find "documents" {:hello "world"})
+(mc/find-maps "documents" {:hello {$exists true}})
