e5fb1fad70133476a965a94ee979f670a28dd2f4
Thomas Henderson <tom@mathpunk.net>
2013-08-14 10:53:38 -0700

    re-ordering snippets

--- src/livre/server/inventory.clj
+++ src/livre/server/inventory.clj
@@ -11,20 +11,39 @@
 ; * should suck less
 
 
-; Connecting
-(ns livre.server
-  (:require [monger.core :as mg])
-  (:import [com.mongodb MongoOptions ServerAddress]))
-
-; with ObjectID
-
 (ns livre.server
   (:require [monger.collection :as m])
   (:use [monger.core :only [connect! connect set-db! get-db]]
         [monger.operators])
   (:import [org.bson.types ObjectId] 
-        [com.mongodb DB WriteConcern]))
+        [com.mongodb DB WriteConcern MongoOptions ServerAddress]))
 
+; connect
+;; given host, given port
+(mg/connect! { :host "localhost" :port 27017 })
+
+;; using MongoOptions allows fine-tuning connection parameters,
+;; like automatic reconnection (highly recommended for production environment)
+(let [^MongoOptions opts (mg/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
+      ^ServerAddress sa  (mg/server-address "127.0.0.1" 27017)]
+  (mg/connect! sa opts))
+
+
+;; localhost, default port
+(mg/connect!)
+(mg/set-db! (mg/get-db "units"))
+
+
+; turn target directory into values, adding ObjectIDs
+;
+; insert collection
+;
+; verify
+;
+; celebrate
+
+
+; ## Value-ing files
 (defn- value-of-a-text-file [file]
      {
        :title       (.getName file)
@@ -39,7 +58,9 @@
 ; :title nil
 ; :ws nil
 ; :content {:uri blah} ?
+;
 
+; ## Working with Directories
 (defn- transform-text-directory-to-values [dirname]
   (let [dir (clojure.java.io/file dirname),
         files (filter #(.isFile %) (file-seq dir))]
@@ -53,9 +74,6 @@
     (map #(str tag % "\n") values))
   )
 
-
-
-
 ; testing
 ; --------------------------------------------------- 
 (def full-test-dir "/home/thomas/src/livre/material/")
@@ -63,70 +81,20 @@
 (def text-test-dir "/home/thomas/src/livre/material/vimwiki")
 
 
-; sanity docs
-; * what am i doing?
-; you are...
-; you need to get your documents into the database. 
-; you know how to convert your documents into values.
-; you have a snippet on adding an object id to a document.
-; you made a connection to a database.
-; you have added documents to the database.
-;
-;
-; algorithm:
-;
-; load monger namespace and requirements
-; (remove edn requirement)
-; 
-; turn target directory into values, adding ObjectIDs
-;
-; insert collection
-;
-; verify
-;
-; celebrate
 
-
-;; given host, given port
-(mg/connect! { :host "localhost" :port 27017 })
-
-;; using MongoOptions allows fine-tuning connection parameters,
-;; like automatic reconnection (highly recommended for production environment)
-(let [^MongoOptions opts (mg/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
-      ^ServerAddress sa  (mg/server-address "127.0.0.1" 27017)]
-  (mg/connect! sa opts))
-
-(get-collection-names)
-
-
-;; localhost, default port
-
-(mg/connect!)
-
-(mg/set-db! (mg/get-db "units"))
-
+; ## Inserting documents and batches
 
 (insert "documents" { :_id (ObjectId.) :hello "world" })
-
-;; multiple documents at once
 (insert-batch "document" [{ :first_name "John" :last_name "Lennon" }
-; ...is a recommended way of inserting batches of documents (from tens to hundreds
-; of thousands) because it is very efficient compared to sequentially or even
-; concurrently inserting documents one by one.
                           { :first_name "Paul" :last_name "McCartney" }])
 
 ;; without document id (when you don't need to use it after storing the document)
 (insert "document" { :first_name "John" :last_name "Lennon" })
 
+(m/insert "documents" {:hello  "dolly" })
 
+(m/insert "documents" {:hello "nasty" })
 
+(m/insert "documents" {:hello "nurse"})
 
-
-
-
-(mc/insert "documents" {:hello  "dolly" })
-(mc/insert "documents" {:hello "nasty" })
-(mc/insert "documents" {:hello "nurse"})
-
-(mc/find "documents" {:hello "world"})
-(mc/find-maps "documents" {:hello {$exists true}})
+(m/find-maps "documents" {:hello {$exists true}})
---
+++ src/livre/server/to-edn.clj
@@ -0,0 +1,11 @@
+(ns livre.server.inventory
+  (:require [clojure.edn :as edn]))
+
+(defn local-dir-to-local-edn [dirname output-name] 
+    (let [data (tagged-value-of-a-directory dirname)
+          output (clojure.java.io/file output-name)]
+      (map #(spit output % :append true) data)
+     )
+  )
+
+(local-dir-to-local-edn text-test-dir "/home/thomas/src/livre/data/test.edn")
