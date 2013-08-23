0e9e730995fb2e33ab28e98ecf1bb05e6ff780ad
Thomas Henderson <tom@mathpunk.net>
2013-08-22 17:29:37 -0700

    did some stuff

--- scrap-code/rb-graphviz/dotfiler.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby 
-
-command = ARGV[0] || 'dot'
-file = ARGV[1] || 'antioutline1.gv'
-image_type = 'jpg'
-
-`#{command} -T#{image_type} #{file} > #{file}.#{command}.#{image_type}`
