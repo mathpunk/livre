4ae717b9c3f000e093166f3a9c0508c19e04dd67
Thomas Henderson <tom@mathpunk.net>
2013-08-28 13:38:37 -0700

    Word counting functions

--- src/livre/models/corpus.clj
+++ src/livre/models/corpus.clj
@@ -1,22 +1,52 @@
 (ns livre.corpus
-  (:use [livre.db :only (article-space)]))
+  (:use [livre.db :only (article-space)])
+  (:require [monger.collection :as m] [monger.core :as mc] [monger.query :as q]))
 
 (def corpus (article-space "/home/thomas/src/livre/material/wiki"))
 
-; Goal: 
-; Access the corpus abstractly.
-; What's a corpus got?
-;
-; articles - textons that have names
-; heds
-; stanzas
-; lines
-; words
-;
-; That's great, except that right now these things come from my filesystem, and later
-; these things come from my db. I want to abstract away the details of where these textons
-; "are". 
+;; # STATISTICS
+;; ## articles (i.e., textons with names)
+(def number-of-articles (count corpus))
 
+;; ## words
+(defn gather-words 
+   "Given a string, return a list of lower-case words with whitespace and
+    punctuation removed.  --http://blog.darevay.com/2010/10/remedial-clojure-leiningen-lazytest-and-some-code/"
+   [s]
+   (map #(.toLowerCase %) (filter #(not (.isEmpty %)) (seq (.split #"[\s\W]+" s)))))
+
+(defn wc
+  [article]
+  (count (gather-words (article :content))))
+
+(defn number-of-words
+  [corpus]
+  (let [collection (map (fn [article] (wc article)) corpus)]
+    (reduce + collection)))
+
+(def wc-total
+  (number-of-words corpus))
+
+;; ## lines
+
+
+(defn lines 
+  [article]
+  (-> (article :content)
+      (
+
+;; ## headings
+
+(defn heds 
+  (fn 
+    ([article] ; filter lines 
+    ([article search]
+
+
+;; ## stanzas
+
+
+;; # PATTERN MATCHING
 ; this is just for playing with strings
 (defn toy-corpus
   [content]
@@ -25,43 +55,3 @@
     ([pattern] (re-seq pattern content))))
 
 ; what would the real one look like? well, it has to take some source for data...
-(defn corpus
-  [location]
-  ; maybe it's a local directory
-  (try
-       (clojure.java.io/file location))
-  ; or maybe it's a mongo connection
-  ; (try ???????)
-
-
-; CORPUS FILTERS
-; So you've got the corpus. What substructures would you want?
-  ;
-  ; articles (which have names, btw)
-  ; heds
-  ; lines
-  ; words
-  ;
-; and those ought to chain, so you can get the words of the filtered articles of the corpus, etc.
-  
-  
-
-
-  
-
-(ns livre.db
-  (:require [monger.collection :as m] [monger.core :as mc] [monger.query :as q])
-(defn lines 
-  [article]
-  (-> (article :content)
-      (
-
-
-(defn heds 
-  (fn 
-    ([article] ; filter lines 
-    ([article search]
-
-
-
-(def corpus (article-space "/home/thomas/src/livre/material/wiki"))
