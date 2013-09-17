b22ae6c01599b82df4cff5aa49ac1a38eed0645d
Thomas Henderson <tom@mathpunk.net>
2013-09-16 19:35:50 -0700

    Connected through monger (db livre) through feuille

--- src/livre/db.clj
+++ src/livre/db.clj
@@ -4,9 +4,10 @@
   (:import [org.bson.types ObjectId] [com.mongodb DB WriteConcern MongoOptions ServerAddress]))
 
 ;; DB/Connection-- recommended options
-(defn connect-to-mongo [db]
+(defn connection [host db]
   (let [^MongoOptions opts (mc/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
-        ^ServerAddress sa  (mc/server-address "127.0.0.1" 27017)]
+        port 27017
+        ^ServerAddress sa  (mc/server-address host port)]
     (do
       (mc/connect! sa opts)
       (mc/set-db! (mc/get-db db))
@@ -14,45 +15,5 @@
     )
   )
 
-
-;; Article: records
-(defn article [file]
-  (let [oid (ObjectId.)] 
-     {
-       :_id         oid
-       :title       (.getName file)
-       :path        (.getParent file)
-       :content     (slurp file)
-       :history     [{:tx (.lastModified file) :diff nil}]
-      }
-    )
-  )
-
-;; Article space: many records
-(defn article-space [dirname]
-  (let [dir (clojure.java.io/file dirname),
-        files (filter #(.isFile %) (file-seq dir))]
-      (map article (remove #(.isHidden %) files))
-    )
-  )
-
-
-;; Rebase many records into a database of some name
-(defn rebase-text-data [dirname dbname]
-  (let [values (article-space dirname)]
-    (do
-      (m/insert-batch dbname values))          
-    )
-  )
-
-;; Mongo
-;; I'm not sure that the stuff is getting into the db/collection because I keep forgetting how to 
-;; get stuff out of a db/collection. Try this? 
-;; 
-;; As for "doing things" right now I'm just using a big dictionary as my corpus. See models/corpus.clj.
-;; (connect-to-mongo "livre")
-
-;; (m/insert "articles" (article "/home/thomas/src/livre/material/wiki/test.wiki"))
-
-;; (connect! { :host "feuille" :port 27017 })                  ;; I included this commented line for... reasons?
-;; (rebase-text-data "units" "articles")
+(defn connect [ ]
+  (connection "feuille" "livre"))
