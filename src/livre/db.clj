128c9bf1d5977dd8acf80e753b3b34f774530c52
Thomas Henderson <tom@mathpunk.net>
2013-09-18 04:15:28 -0700

    fixed oid somehow

--- src/livre/db.clj
+++ src/livre/db.clj
@@ -3,7 +3,7 @@
   (:use [monger.operators])
   (:import [org.bson.types ObjectId] [com.mongodb DB WriteConcern MongoOptions ServerAddress]))
 
-;; DB/Connection-- recommended options
+;; Database defaults
 (defn connection [host db]
   (let [^MongoOptions opts (mc/mongo-options :threads-allowed-to-block-for-connection-multiplier 300)
         port 27017
@@ -15,12 +15,34 @@
     )
   )
 
+;; Connect to my db
 (defn connect [ ]
   (connection "feuille" "livre"))
 
+;; A record: "article"
+(defn article [file]
+  (let [oid (ObjectId.)] 
+     {
+       :_id         oid
+       :title       (.getName file)
+       :path        (.getParent file)
+       :content     (slurp file)
+       :history     [{:tx (.lastModified file) :diff nil}]
+      }
+    )
+  )
 
-;;;; test
+;; A mess of records: "articles"
+(defn articles [dirname]
+  (let [dir (clojure.java.io/file dirname),
+        files (filter #(.isFile %) (file-seq dir))]
+      (map article (remove #(.isHidden %) files))
+    )
+  )
+
+;; Rebase many records into a database of some name
+(defn rebase-articles [dirname]
+  (let [values (articles dirname)]
+    (m/insert-batch "articles" values)))
 
 (connect)
-
-(first (m/find-maps "annotations"))
--- src/livre/models/queries.clj
+++ src/livre/models/queries.clj
@@ -4,3 +4,70 @@
 ;;
 (count (m/find-maps "document" {:ws {$exists true}}))
 
+;; articles (i.e., textons with names)
+
+; (def number-of-articles (count corpus))
+
+;; headings
+; (defn heds 
+;   (fn 
+;     ([article] ; filter lines 
+;     ([article search]
+
+;; stanzas
+; (defn stanzas
+
+;; lines
+; (defn lines 
+;   [article]
+;   (-> (article :content)
+;       (
+
+;; words
+(defn gather-words 
+   "Given a string, return a list of lower-case words with whitespace and
+    punctuation removed.  --http://blog.darevay.com/2010/10/remedial-clojure-leiningen-lazytest-and-some-code/"
+   [s]
+   (map #(.toLowerCase %) (filter #(not (.isEmpty %)) (seq (.split #"[\s\W]+" s)))))
+
+(defn wc
+  [article]
+  (count (gather-words (article :content))))
+
+(defn number-of-words
+  [corpus]
+  (let [collection (map (fn [article] (wc article)) corpus)]
+    (reduce + collection)))
+
+(def wc-total
+  (number-of-words corpus))
+
+;; corpuscles
+(defn attract [fielded-texton]
+  (fn [another-fielded-texton]
+    ; search back and forth among their keys
+    ))
+
+; for corpus,
+; create a vector of titles
+
+; for each title,
+; create vector of headers
+
+; for each header, 
+; create a vector of stanzas
+
+; for each stanza,
+; create a vector of lines
+
+; for each line,
+; create a vector of words
+
+
+(m/find-maps "annotations")
+(m/insert "articles" (article "/home/thomas/src/livre/material/wiki/test.wiki"))
+(mc/connect! { :host "feuille" :port 27017 })  
+(m/find)
+
+
+
